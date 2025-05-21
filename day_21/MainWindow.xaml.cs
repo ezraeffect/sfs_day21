1148cbed303085be0a835e7accb707fcd3ebe4c6
ezraeffect <build@fx0.kr>
2025-05-21 13:37:50 +0900

    init: MainWindow UI Xaml

--- day_21/MainWindow.xaml.cs
+++ day_21/MainWindow.xaml.cs
@@ -14,11 +14,90 @@ namespace day_21
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    /*
+     * 실습. 컬러 슬라이더
+     * • 슬라이더 3개를 넣고 각각 Label로 Red, Green, Blue 이름을 붙임
+     * • 범위는 0~255를 가지고, 5 단위로 움직임
+     * • 각 슬라이더의 현재 값을 Label 또는 TextBox로 표현 
+     * • 값을 변경할 때마다 지정된 값 대로 Grid의 배경색 변경
+     * • Color.FromRgb() 메서드 검색
+     * • SolidColorBrush() 검색
+     * • 그룹 박스 안에 일반, 반전, 흑백 라디오 버튼을 각각 만들고 해당 기능에 따라 배경 색상이 바뀌도록 
+     * • 흑백은 RGB 값을 평균내서 모든 컬러 채널에 일괄 적용
+     * • 반전은 RGB 각각의 컬러 채널에서 최대값인 255에서 현재 값을 뺀 값을 적용
+     */
     public partial class MainWindow : Window
     {
+        public RGB color = new RGB();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            
+        }
+
+        public class RGB
+        {
+            public int Red {  get; set; }
+            public int Green { get; set; }
+            public int Blue { get; set; }
+
+            public RGB()
+            {
+                Red = 0;
+                Green = 0;
+                Blue = 0;
+            }
+            public RGB(int red, int green, int blue)
+            {
+                Red = red;
+                Green = green;
+                Blue = blue;
+            }
+
+            public void ConvertOriginal(int r, int g, int b)
+            {
+                this.Red = r;
+                this.Green = g;
+                this.Blue = b;
+            }
+
+            public void ConvertGrayTone(int r, int g, int b)
+            {
+                // 흑백은 RGB 값을 평균내서 모든 컬러 채널에 일괄 적용
+                int gray = (int)Math.Round(0.299 * r + 0.587 * g + 0.114 * b);
+                this.Red = gray;
+                this.Green = gray;
+                this.Blue = gray;
+            }
+
+            public void ConvertInvert(int r, int g, int b)
+            {
+                // 반전은 RGB 각각의 컬러 채널에서 최대값인 255에서 현재 값을 뺀 값을 적용
+                this.Red = 255 - r;
+                this.Green = 255 - g;
+                this.Blue = 255 - b;
+            }
+        }
+
+        private void sliderRed_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            color.Red = (int)sliderRed.Value;
+            textBlockRed.Text = color.Red.ToString();
+        }
+
+        private void sliderGreen_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            color.Green = (int)sliderGreen.Value;
+            textBlockGreen.Text = color.Green.ToString();
+        }
+
+        private void sliderBlue_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            color.Blue = (int)sliderBlue.Value;
+            textBlockGreen.Text= color.Blue.ToString();
         }
     }
 }
