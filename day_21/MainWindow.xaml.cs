34999f4119a31ccbc3918247ce1f125c4d66855f
Jeonghun Lee <build@fx0.kr>
2025-05-21 20:48:42 +0900

    fix: 색상 변경 로직, Result Rectangle 추가

--- day_21/MainWindow.xaml.cs
+++ day_21/MainWindow.xaml.cs
@@ -5,9 +5,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace day_21
 {
@@ -22,75 +24,106 @@ namespace day_21
      * • 각 슬라이더의 현재 값을 Label 또는 TextBox로 표현 
      * • 값을 변경할 때마다 지정된 값 대로 Grid의 배경색 변경
      * • Color.FromRgb() 메서드 검색
-     * • SolidColorBrush() 검색
+     * • idColorBrush() 검색
      * • 그룹 박스 안에 일반, 반전, 흑백 라디오 버튼을 각각 만들고 해당 기능에 따라 배경 색상이 바뀌도록 
      * • 흑백은 RGB 값을 평균내서 모든 컬러 채널에 일괄 적용
      * • 반전은 RGB 각각의 컬러 채널에서 최대값인 255에서 현재 값을 뺀 값을 적용
      */
+
+    public enum ColorType
+    {
+        None = 0,       // 0 : None
+        Original = 1,   // 1 : 원본
+        GrayTone = 2,   // 2 : 회색조
+        Invert = 3      // 3 : 반전
+    }
+
     public partial class MainWindow : Window
     {
-        public RGB color = new RGB();
+        public RGB color = new();
 
         public MainWindow()
         {
             InitializeComponent();
-
-
         }
 
-        private void UpdateBackground()
+        private void UpdateBackground(Color color)
         {
-            bgGrid.Background = new SolidColorBrush(color.ToColor());
+            bgResult.Fill = new SolidColorBrush(color);
+            
         }
 
         public class RGB
         {
-            public int Red { get; set; }
-            public int Green { get; set; }
-            public int Blue { get; set; }
+            public byte Red { get; set; }
+            public byte Green { get; set; }
+            public byte Blue { get; set; }
+            public ColorType cType { get; set; }
+            public Color ConvertedColor { get; set; }
 
+
+            // Class 생성자 (매개변수 없을 시 0,0,0으로 설정)
             public RGB()
             {
                 Red = 0;
                 Green = 0;
                 Blue = 0;
+                cType = ColorType.Original;
+                ConvertedColor = Color.FromRgb(0, 0, 0);
             }
-            public RGB(int red, int green, int blue)
+
+            // Class 생성자 (R, G, B 값을 매개변수로 설정)
+            public RGB(byte red, byte green, byte blue)
             {
                 Red = red;
                 Green = green;
                 Blue = blue;
+                cType = ColorType.Original;
+                ConvertedColor = Color.FromRgb(red, green, blue);
             }
 
-            public void ConvertOriginal(int r, int g, int b)
+            // Class의 ColorType 속성에 따라 색상을 변환하는 method
+            public void Convert(ColorType cType)
             {
-                this.Red = r;
-                this.Green = g;
-                this.Blue = b;
+                switch (cType)
+                {
+                    case ColorType.None:
+                        ConvertedColor = this.ToColor();
+                        break;
+                    case ColorType.Original:
+                        ConvertedColor = this.ToColor();
+                        break;
+                    case ColorType.GrayTone:
+                        // 흑백은 RGB 값을 평균내서 모든 컬러 채널에 일괄 적용
+                        byte gray = (byte)Math.Round(0.299 * Red + 0.587 * Green + 0.114 * Blue);
+
+                        ConvertedColor = this.ToColor(gray, gray, gray);
+                        break;
+                    case ColorType.Invert:
+                        // 반전은 RGB 각각의 컬러 채널에서 최대값인 255에서 현재 값을 뺀 값을 적용
+                        byte r = (byte)(255 - Red);
+                        byte g = (byte)(255 - Green);
+                        byte b = (byte)(255 - Blue);
+
+                        ConvertedColor = this.ToColor(r, g, b);
+                        break;
+                }
             }
 
-            public void ConvertGrayTone(int r, int g, int b)
-            {
-                // 흑백은 RGB 값을 평균내서 모든 컬러 채널에 일괄 적용
-                int gray = (int)Math.Round(0.299 * r + 0.587 * g + 0.114 * b);
-                this.Red = gray;
-                this.Green = gray;
-                this.Blue = gray;
-            }
-
-            public void ConvertInvert(int r, int g, int b)
-            {
-                // 반전은 RGB 각각의 컬러 채널에서 최대값인 255에서 현재 값을 뺀 값을 적용
-                this.Red = 255 - r;
-                this.Green = 255 - g;
-                this.Blue = 255 - b;
-            }
-
+            // Class의 RGB 값을 Color 개체로 변환하는 method
             public Color ToColor()
             {
-                return Color.FromRgb((byte)this.Red, (byte)this.Green, (byte)this.Blue);
+                return Color.FromRgb(this.Red, this.Green, this.Blue);
             }
 
+            // 매개변수로 입력된 RGB 값을 Color 개체로 변환하는 method (overload로 구현)
+            public Color ToColor(byte r, byte g, byte b)
+            {
+                return Color.FromRgb(r, g, b);
+            }
+
+
+            // 입력된 색상을 출력하는 Debugging method
             public void PrintColor()
             {
                 Console.WriteLine("Color Info");
@@ -106,26 +139,51 @@ namespace day_21
 
             if (slider.Tag is string tag)
             {
-                int value = (int)slider.Value;
+                byte value = (byte)slider.Value;
 
                 if (tag == "Red")
                 {
                     color.Red = value;
-                    textBlockRed.Text = color.Red.ToString();
+                    textBlockRed.Text = value.ToString();
                 }
                 else if (tag == "Green")
                 {
                     color.Green = value;
-                    textBlockGreen.Text = color.Green.ToString();
+                    textBlockGreen.Text = value.ToString();
                 }
                 else if (tag == "Blue")
                 {
                     color.Blue = value;
-                    textBlockBlue.Text = color.Blue.ToString();
+                    textBlockBlue.Text = value.ToString();
                 }
             }
 
-            UpdateBackground();
+            color.Convert(color.cType);
+            UpdateBackground(color.ConvertedColor);
+        }
+
+        private void radioColorType_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = (RadioButton)sender;
+
+            if (rb.Tag is string tag)
+            {
+                if (tag == "Original")
+                {
+                    color.cType = ColorType.Original;
+                }
+                else if (tag == "GrayTone")
+                {
+                    color.cType = ColorType.GrayTone;
+                }
+                else if (tag == "Invert")
+                {
+                    color.cType = ColorType.Invert;
+                }
+            }
+
+            color.Convert(color.cType);
+            UpdateBackground(color.ConvertedColor);
         }
     }
 }
