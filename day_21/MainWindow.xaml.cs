5e769a00d52082de0e3f0e79e3423670b4ca200d
ezraeffect <build@fx0.kr>
2025-05-21 12:53:33 +0900

    add: Textbox, GroupBox, RadioButton, Slider

--- day_21/MainWindow.xaml.cs
+++ day_21/MainWindow.xaml.cs
@@ -56,6 +56,36 @@ using System.Windows.Shapes;
  * - 사용자 직접 입력 X (입력은 TextBox)
  */
 
+/*
+ * GroupBox
+ * 여러 개의 UI 요소를 한의 그룹으로 묶어주고, 그 그룹에 제목(label)을 붙일 수 있는 컨테이너
+ * - 관련된 항목들을 시각적으로 묶어 표현할 때 사용
+ */
+
+/*
+ * RadioButton
+ * 
+ * [Attribute]
+ * GroupName : 여러 개의 라디오 버튼이 서로 다른 그룹으로 동작하도록 구분하는 속성
+ * - 같은 GroupName을 가진 버튼들끼리는 서로 하나만 선택 가능
+ * - 다른 GroupName을 부여하면 동시 선택 가능
+ * Checked : RadioButton이 선택 될 때 연결되는 이벤트 핸들러
+ * IsChecked : 현재 체크 상태를 코드로 확인할 때 사용
+ */
+
+/*
+ * Slider
+ * 사용자가 들래그하거나 클릭해서 숫자 값을 조절 할 수 있도록 해주는 입력 컨트롤
+ * 
+ * [Attribute]
+ * - Minimum : 최소 값 (Default 0)
+ * - Maximum : 최대 값 (Default 10)
+ * - Value : 현재 값 (Default 0)
+ * - Orientation : 방향 설정
+ * - TickFrequency : 눈금 간격 설정
+ * - IsSnapToTickEnabled : 눈금에 맞춰 조절할지 여부
+ * - Ticks : 눈금 위치 설정
+ */
 namespace day_21
 {
     /// <summary>
@@ -67,5 +97,43 @@ namespace day_21
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // 선택된 RadioButton 확인
+            string result = string.Empty;
+
+            if (radioMale.IsChecked == true)
+            {
+                result = "남성을 선택했습니다.";
+            }
+            else if (radioFemale.IsChecked == true)
+            {
+                result = "여성을 선택했습니다.";
+            }
+
+            MessageBox.Show(result);
+        }
+
+        private void volumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // Slider Event
+            if (volumeText.Text != null)
+            {
+                volumeText.Text = $"현재 값 : {volumeSlider.Value}";
+            }
+        }
+
+        /*
+        private void radioMale_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("남성을 선택했습니다.");
+        }
+
+        private void radioFemale_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("여성을 선택했습니다.");
+        }
+        */
     }
 }
